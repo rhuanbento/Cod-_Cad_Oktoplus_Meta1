d085a8f0c2731cf28fb11efee8593ad86430d8d5
rhuan <rhnbnt@gmail.com>
2022-04-03 14:59:57 -0300

    ...

--- Estrutura De Dados/Times.cpp
+++ Estrutura De Dados/Times.cpp
@@ -12,8 +12,11 @@ int main(){
     cin >> N >> T;
 
    vector <pair <string, int>> alunos;
+   pair<string, int> verifica;
 
    for(int i = 0; i < alunos.size(); i++){
+       cin >> verifica.first >> verifica.second;
+       alunos.push_back(verifica);
        
    }
 
